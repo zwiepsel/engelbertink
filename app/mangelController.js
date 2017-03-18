adb64521d8af05fd1b24efbf2ff677c72ed333a7
zwiepsel <zwiepsel@gmail.com>
2017-03-18 11:02:22 +0100

    2e aanpassingen

--- app/mangelController.js
+++ app/mangelController.js
@@ -10,8 +10,16 @@
   function MangelController(LocalStorage, QueryService, $scope, $routeParams) {
 
     // 'controller as' syntax
+    console.log('hoi')
+    $scope.tonen = false;
+    $scope.niettonen = true;
     var self = this;
     $scope.mangel = $routeParams.id;
+
+    $scope.zoekKlant = function(){
+      $scope.tonen = true;
+      $scope.niettonen = false;
+    }
 
   }
 
